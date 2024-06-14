87e8c360eebb18c99097319a231320dfb8972509
TanV-04 <aretanvi@gmail.com>
2024-06-14 23:09:21 +0530

    tanvi_sontakke

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
-    import "../app.css";
+  import "../app.css";
 </script>
 
 <slot />
+
+<!--a common file that exists across all files, especially when you're working with a complex
+application-->
