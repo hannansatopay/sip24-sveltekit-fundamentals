a63a71ec22fa0c890b30c53bde413a1584a435ca
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-14 22:22:01 +0530

    Completed step 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
-<header class="bg-white py-4 shadow-md">
-    <div class="container mx-auto px-4">
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+    <div class="container mx-auto px-4 flex justify-between items-center">
 
         <h1 class="text-2xt font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+        <a href="/add-post" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add post</a>
     </div>
 </header>
 
