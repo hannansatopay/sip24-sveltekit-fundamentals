e5f618b84c628ffb2f7a48e4fc393503c1bb1fc9
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-14 21:42:15 +0530

    First step completed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,35 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="bg-white py-4 shadow-md">
+    <div class="container mx-auto px-4">
+
+        <h1 class="text-2xt font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+    </div>
+</header>
+
+<div class="container mx-auto md:my-5">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <!-- Post template -->
+        <div>
+            <img class="w-full" src="https://picsum.photos/600" alt="post">
+            <div class="text-sm py-2">
+                <span class="font-bold">Username</span>
+                <span class="text-gray-700">Lorem Ipsom Doler Ismet</span>
+            </div>
+        </div>
+        <div>
+            <img class="w-full" src="https://picsum.photos/600" alt="post">
+            <div class="text-sm py-2">
+                <span class="font-bold">Username</span>
+                <span class="text-gray-700">Lorem Ipsom Doler Ismet</span>
+            </div>
+        </div>
+        <div>
+            <img class="w-full" src="https://picsum.photos/600" alt="post">
+            <div class="text-sm py-2">
+                <span class="font-bold">Username</span>
+                <span class="text-gray-700">Lorem Ipsom Doler Ismet</span>
+            </div>
+        </div>
+
+    </div>
+
+</div>
