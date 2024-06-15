196344c3c2805c8c873081e2e8f360f44d299677
Anuraj Venkatpurwar <anurajvenkatpurwar@gmail.com>
2024-06-15 17:18:55 +0530

    Successfully implemented share post svelte app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<script>
+    export let data;
+</script>
 <header class="bg-white py-4 shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex justify-between items-center">
 
@@ -8,28 +11,16 @@
 
 <div class="container mx-auto md:my-5">
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <!-- Post template -->
+        {#each data.posts as post}
         <div>
             <img class="w-full" src="https://picsum.photos/600" alt="post">
             <div class="text-sm py-2">
-                <span class="font-bold">Username</span>
-                <span class="text-gray-700">Lorem Ipsom Doler Ismet</span>
+                <span class="font-bold">{post.username}</span>
+                <span class="text-gray-700">{post.content}</span>
             </div>
         </div>
-        <div>
-            <img class="w-full" src="https://picsum.photos/600" alt="post">
-            <div class="text-sm py-2">
-                <span class="font-bold">Username</span>
-                <span class="text-gray-700">Lorem Ipsom Doler Ismet</span>
-            </div>
-        </div>
-        <div>
-            <img class="w-full" src="https://picsum.photos/600" alt="post">
-            <div class="text-sm py-2">
-                <span class="font-bold">Username</span>
-                <span class="text-gray-700">Lorem Ipsom Doler Ismet</span>
-            </div>
-        </div>
+        {/each}
+        
 
     </div>
 
