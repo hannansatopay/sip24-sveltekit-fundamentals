002fafc9130b5d94689f24f07efa95510bae23c5
Bright Byamukama <byamukamabright@outlook.com>
2024-06-15 17:50:40 +0300

    second commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,33 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+        <h1 class="text-2xl font-bold font-['Comic_sans_MS']">CraftLab</h1>
+        <a  href="/add-post" class="bg-blue-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-blue-700">Add Post </a>
+    </div>
+</header>
+<div class="container mx-auto md:my-5">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <!-- post Template-->
+         <div>
+            <img class="w-full" src="https://picsum.photos/600" alt="post">
+            <div class="text-sm py-2">
+                <span class="fontbold">Username</span>
+                <span class="text-gray-700">Lorem ipsum dolor sit amet consectetur</span>
+            </div>
+         </div>
+         <div>
+            <img class="w-full" src="https://picsum.photos/600" alt="post">
+            <div class="text-sm py-2">
+                <span class="fontbold">Username</span>
+                <span class="text-gray-700">Lorem ipsum dolor sit amet consectetur</span>
+            </div>
+         </div>
+         <div>
+            <img class="w-full" src="https://picsum.photos/600" alt="post">
+            <div class="text-sm py-2">
+                <span class="fontbold">Username</span>
+                <span class="text-gray-700">Lorem ipsum dolor sit amet consectetur</span>
+            </div>   
+         </div>
+
+    </div>
+</div>
