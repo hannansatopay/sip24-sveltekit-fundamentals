4ed066e0e31eb856033edc531d9915e220b3ebe5
Farhan Sayed <162037189+FarhanSayed16@users.noreply.github.com>
2024-06-16 16:07:50 +0530

    update-dark

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,30 +7,28 @@
         posts = await response.json();
     });
 </script>
-
-<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+<header class="bg-gray-800 py-4 shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex justify-between items-center">
-        <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+        <h1 class="text-2xl font-bold text-white font-['Comic_Sans_MS']">Craftlab</h1>
         <a
             href="/add-post"
-            class="bg-blue-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded"
-            >Add post</a
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >Add Post</a
         >
     </div>
 </header>
-
-<div class="container mx-auto">
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+<div class="container mx-auto py-6">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         {#each posts as post}
-            <div>
+            <div class="bg-gray-700 rounded-lg overflow-hidden shadow-lg">
                 <img
                     class="w-full"
                     src={`data:image/jpeg;base64,${post.image}`}
                     alt="Post"
                 />
-                <div class="text-sm py-2">
-                    <span class="font-bold">{post.username}</span>
-                    <span class="text-gray-700">{post.content}</span>
+                <div class="p-4">
+                    <div class="text-white text-lg font-bold">{post.username}</div>
+                    <div class="text-gray-300">{post.content}</div>
                 </div>
             </div>
         {/each}
--- src/routes/add-post/+page.svelte
+++ src/routes/add-post/+page.svelte
@@ -2,7 +2,6 @@
     let files = null;
     let previewUrl = null;
 
-    // Reactive statement to update previewUrl when files change
     $: if (files && files.length > 0) {
         const file = files[0];
         previewUrl = URL.createObjectURL(file);
@@ -11,28 +10,35 @@
     }
 </script>
 
-<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+<header class="bg-gray-800 py-4 shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex justify-between items-center">
-        <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+        <h1 class="text-2xl font-bold text-white font-['Comic_Sans_MS']">
+            Craftlab
+        </h1>
         <a
             href="/"
-            class="bg-blue-500 hover:bg-red-300 text-white font-bold py-2 px-4 rounded"
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >HOME</a
         >
     </div>
 </header>
-
-<form class="container mx-auto p-5" method="POST" enctype="multipart/form-data">
+<form
+    class="container mx-auto p-5 bg-gray-700 rounded-lg shadow-lg"
+    method="POST"
+    enctype="multipart/form-data"
+>
     <label
         for="dropzone"
-        class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
+        class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-500 border-dashed rounded-lg cursor-pointer bg-gray-600"
     >
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+        <div
+            class="flex flex-col items-center justify-center pt-5 pb-6 text-white"
+        >
             {#if previewUrl}
                 <img src={previewUrl} alt="Preview" class="max-h-64 mb-4" />
             {:else}
                 <svg
-                    class="w-8 h-8 mb-4 text-gray-500"
+                    class="w-8 h-8 mb-4 text-gray-400"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -45,7 +51,7 @@
                         d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                     /></svg
                 >
-                <p class="text-sm text-gray-500 font-semibold">
+                <p class="text-sm text-gray-400 font-semibold">
                     Click to upload
                 </p>
             {/if}
@@ -63,24 +69,24 @@
     <div class="mb-3">
         <label
             for="username"
-            class="block mb-2 text-sm font-medium text-gray-900">Username</label
+            class="block mb-2 text-sm font-medium text-gray-200">Username</label
         >
         <input
             name="username"
             id="username"
             type="text"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            class="bg-gray-600 border border-gray-500 text-gray-200 text-sm rounded-lg block w-full p-2.5"
         />
     </div>
     <div class="mb-3">
         <label
             for="content"
-            class="block mb-2 text-sm font-medium text-gray-900">Content</label
+            class="block mb-2 text-sm font-medium text-gray-200">Content</label
         >
         <textarea
             name="content"
             id="content"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            class="bg-gray-600 border border-gray-500 text-gray-200 text-sm rounded-lg block w-full p-2.5"
         ></textarea>
     </div>
     <button
