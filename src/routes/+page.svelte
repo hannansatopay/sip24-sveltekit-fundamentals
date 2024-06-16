75f50e80f4b588c4c6661c679a682d85466cb53f
Garuna414 <anuragnalkar414@gmail.com>
2024-06-16 22:39:04 +0530

    Additional task part
    Implemented the additional task of applying filter using html5 canvas.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,24 +13,19 @@
 </header>
 
 <div
-  class="container max-w-[425px] items-center mx-auto my-5 bg-gray-200 grid grid-cols-1 gap-2 divide-y divide-black"
+  class="container max-w-[425px] items-center mx-auto my-5 bg-gray-200 grid grid-cols-1 gap-2 divide-y border border-black rounded-lg"
 >
   <div class="grid grid-cols-1 divide-y divide-blue-500">
-    {#each data.posts as post}
-      <div>
-        <p class="px-2 text-sm font-mono">{post.username}</p>
-        <img src="data:image;base64,{post.image}" alt="Post" class="w-full" />
-        <span class="grid grid-cols-[60px_60px_auto] gap-2 px-2">
-          <button
-            class="bg-blue-500 px-2 py-1 my-1 text-sm font-mono rounded-lg hover:scale-105 hover:shadow-sm hover:shadow-black text-white font-bold max-w-16"
-            >Up</button
-          >
-          <button
-            class="bg-red-500 px-2 py-1 my-1 text-sm font-mono rounded-lg hover:scale-105 hover:shadow-sm hover:shadow-black text-white font-bold max-w-16"
-            >Down</button
-          >
-          <span class="py-1 text-sm font-mono">{post.content}</span>
-        </span>
+    {#each data.posts as post, id}
+      <div class="p-2">
+        <p class="text-sm font-mono">{post.username}</p>
+        <img
+          src="data:image;base64,{post.image}"
+          alt="Post"
+          class="w-full"
+          id="{id}"
+        />
+        <span class="py-1 text-sm font-mono">{post.content}</span>
       </div>
       <div class="divide-y divide-blue-500"></div>
     {/each}
--- src/routes/addPost/+page.svelte
+++ src/routes/addPost/+page.svelte
@@ -1,23 +1,90 @@
 <script>
-  let files = null;
+  let files = null,
+    filter = 0;
+  async function showImg(val) {
+    filter = val;
+    const userInput = document.getElementById("dropzoneInp");
+    let file = userInput.files[0];
+    let dpi = window.devicePixelRatio;
+    let canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+    console.log("showImg");
+    function fix_dpi() {
+      let style_height = +getComputedStyle(canvas)
+        .getPropertyValue("height")
+        .slice(0, -2);
+      let style_width = +getComputedStyle(canvas)
+        .getPropertyValue("width")
+        .slice(0, -2);
+      canvas.setAttribute("height", style_height * dpi);
+      canvas.setAttribute("width", style_width * dpi);
+    }
+    if (file) {
+      let imgURL = URL.createObjectURL(file);
+      const img = new Image();
+      img.onload = function () {
+        fix_dpi();
+        var hRatio = canvas.width / img.width;
+        var vRatio = canvas.height / img.height;
+        var ratio = Math.min(hRatio, vRatio);
+        var centerShift_x = (canvas.width - img.width * ratio) / 2;
+        var centerShift_y = (canvas.height - img.height * ratio) / 2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (val == 0) {
+          ctx.filter = "none";
+        } else if (val == 1) {
+          ctx.filter = "grayscale()";
+        } else if (val == 2) {
+          ctx.filter = "sepia()";
+        } else if (val == 3) {
+          ctx.filter = "invert()";
+        }
+        ctx.drawImage(
+          img,
+          0,
+          0,
+          img.width,
+          img.height,
+          centerShift_x,
+          centerShift_y,
+          img.width * ratio,
+          img.height * ratio
+        );
+        canvas.toBlob((blob) => {
+          const newFile = new File([blob], file.name, { type: file.type });
+          const dataTransfer = new DataTransfer();
+          dataTransfer.items.add(newFile);
+          document.getElementById("dropzone").files = dataTransfer.files;
+        }, file.type);
+      };
+      img.src = imgURL;
+    } else {
+      alert("No file detected, please insert a file.");
+    }
+  }
 </script>
 
-<header class="shadow-md shadow-gray-600 sticky z-10 bg-white top-0">
+<header class="shadow-md shadow-gray-600 sticky z-10 bg-white top-0 w-full">
   <div class="flex flex-row justify-between items-center py-2 px-3 font-mono">
     <p class="text-2xl font-bold">Craftlab</p>
-    <a href="/" class="bg-blue-500 px-2 py-1 rounded-lg text-white font-bold"
+    <a
+      href="/"
+      class="bg-blue-500 px-2 py-1 rounded-lg text-white font-bold hover:scale-105 hover:shadow-md hover:shadow-gray-300"
       >Home</a
     >
   </div>
 </header>
 
-<form class="p-5" method="POST" enctype="multipart/form-data">
+<div
+  class="p-5 max-w-[425px] container grid grid-cols-1 items-center mx-auto my-5 bg-gray-200 rounded-lg"
+>
   <label
     for="dropzone"
-    class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
+    class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 p-5"
   >
-    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+    <div class="flex flex-col items-center justify-center">
       {#if files && files.length != 0}
+        <canvas class="w-full h-[225px]" id="canvas"></canvas>
         <p>{files[0].name}</p>
       {:else}
         <svg
@@ -38,15 +105,55 @@
       {/if}
     </div>
     <input
-      name="image"
-      id="dropzone"
+      name="imageInp"
+      id="dropzoneInp"
       type="file"
       accept="image/png, image/jpeg"
-      class="hidden"
+      class="opacity-0 w-full h-full"
       bind:files
+      on:change={() => showImg()}
       required
     />
   </label>
+</div>
+
+<form
+  class="p-5 max-w-[425px] container grid grid-cols-1 items-center mx-auto my-5 bg-gray-200 rounded-lg"
+  method="POST"
+  enctype="multipart/form-data"
+  id="postForm"
+  name="postForm"
+>
+  <input type="file" class="hidden" name="image" id="dropzone" />
+  {#if files && files.length != 0}
+    <div class="flex flex-row mb-5 justify-evenly w-full">
+      <button
+        class="{filter == 0
+          ? 'bg-blue-700'
+          : 'bg-blue-500'} py-2 rounded-lg text-white font-mono w-20 hover:scale-105 hover:shadow-md hover:shadow-gray-400 text-[13px]"
+        on:click={() => showImg(0)}>Original</button
+      >
+      <button
+        class="{filter == 1
+          ? 'bg-blue-700'
+          : 'bg-blue-500'} py-2 rounded-lg text-white font-mono w-20 hover:scale-105 hover:shadow-md hover:shadow-gray-400 text-[13px]"
+        on:click={() => showImg(1)}>Grayscale</button
+      >
+      <button
+        class="{filter == 2
+          ? 'bg-blue-700'
+          : 'bg-blue-500'} py-2 rounded-lg text-white font-mono w-20 hover:scale-105 hover:shadow-md hover:shadow-gray-400 text-[13px]"
+        on:click={() => showImg(2)}>Sepia</button
+      >
+      <button
+        class="{filter == 3
+          ? 'bg-blue-700'
+          : 'bg-blue-500'} py-2 rounded-lg text-white font-mono w-20 hover:scale-105 hover:shadow-md hover:shadow-gray-400 text-[13px]"
+        on:click={() => showImg(3)}>Invert</button
+      >
+    </div>
+  {/if}
+
   <div class="flex flex-col gap-1">
     <label for="username" class="font-mono text-lg">Username</label>
     <input
@@ -54,21 +161,26 @@
       class="bg-gray-100 border border-black rounded-lg w-full h-8 px-3 py-1 font-mono"
       id="username"
       name="username"
+      required
     />
   </div>
   <br />
   <div class="flex flex-col gap-1">
-    <label for="content" class="font-mono text-lg px-3">Content</label>
+    <label for="content" class="font-mono text-lg">Content</label>
     <textarea
       type="text"
       class="bg-gray-100 border border-black rounded-lg w-full h-12 py-1 px-3 font-mono"
       id="content"
       name="content"
+      required
     />
   </div>
   <br />
   <button
-    class="bg-blue-500 px-3 py-2 rounded-lg text-white text-md font-mono font-bold hover:scale-105 hover:shadow-md hover:shadow-gray-400"
+    class="bg-blue-500 px-3 py-2 rounded-lg text-white text-md font-mono font-bold hover:scale-105 hover:shadow-md hover:shadow-gray-400 max-w-20"
     >Post</button
   >
 </form>
+
+<style>
+</style>
