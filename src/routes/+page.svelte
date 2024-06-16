8aeb42d7497decad096d0b3e4149241092a494f9
Kshitish Kumar Pothal <kshitishkumarpothal@gmail.com>
2024-06-16 22:08:48 +0530

    Kshitish
    ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,34 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="py-4 bg-white shadow-md ">
+    <div class="container flex items-center justify-between px-4 mx-auto">
+        <h1 class=" text-2xl font-bold font-['Comic_Sans_MS'] ">MyGram</h1>
+        <a href="/add-post" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Add post</a>
+    </div>
+</header>
+<div class="container mx-auto md:my-5">
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
+        <div>
+            <img class="w-full " src="https://picsum.photos/600" alt="no-bro">
+            <div class="py-2 text-sm ">
+                <span class="font-bold ">username</span>
+                <span class="text-green-700 "> abcdfghijklmnopqrstuvwxyz</span>
+
+            </div>
+        </div>
+        <div>
+            <img class="w-full " src="https://picsum.photos/600" alt="no-bro">
+            <div class="py-2 text-sm ">
+                <span class="font-bold ">username</span>
+                <span class="text-green-700 "> abcdfghijklmnopqrstuvwxyz</span>
+                
+            </div>
+        </div>
+        <div>
+            <img class="w-full " src="https://picsum.photos/600" alt="no-bro">
+            <div class="py-2 text-sm ">
+                <span class="font-bold ">username</span>
+                <span class="text-green-700 "> abcdfghijklmnopqrstuvwxyz</span>
+                
+            </div>
+        </div>
+    </div>
+</div>
