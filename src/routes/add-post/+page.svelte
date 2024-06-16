043bdd34369db82aec22b37d459b0f122f85eb8e
Farhan Sayed <162037189+FarhanSayed16@users.noreply.github.com>
2024-06-16 17:11:17 +0530

    Update 3.0

--- src/routes/add-post/+page.svelte
+++ src/routes/add-post/+page.svelte
@@ -56,6 +56,7 @@
                 </p>
             {/if}
         </div>
+        <!-- damnn, you are reading this carefully!! -->
         <input
             bind:files
             name="image"
@@ -89,6 +90,7 @@
             class="bg-gray-600 border border-gray-500 text-gray-200 text-sm rounded-lg block w-full p-2.5"
         ></textarea>
     </div>
+    <!-- still checking out mate? -->
     <button
         type="submit"
         class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"
