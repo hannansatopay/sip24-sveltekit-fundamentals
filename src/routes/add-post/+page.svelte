a3289d951d2c23b4339a7a338b1844d36b15154e
Farhan Sayed <162037189+FarhanSayed16@users.noreply.github.com>
2024-06-16 16:10:20 +0530

    Update +page.svelte

--- src/routes/add-post/+page.svelte
+++ src/routes/add-post/+page.svelte
@@ -13,7 +13,7 @@
 <header class="bg-gray-800 py-4 shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex justify-between items-center">
         <h1 class="text-2xl font-bold text-white font-['Comic_Sans_MS']">
-            Craftlab
+            Instagram clone-Craftlab
         </h1>
         <a
             href="/"
