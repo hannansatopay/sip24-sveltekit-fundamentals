66ce003023c74d3ee00c84ce2a1a47721ce02303
Chaitanya Reddy <chaitanyareddygujjula@gmail.com>
2024-06-16 22:04:37 +0530

    main task

---
+++ src/routes/add-post/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+    let files= null;
+</script>
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+        <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+        <a href='/' class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Home</a>
+    </div>
+</header>
+
+<form class="contianer mx-auto p-5" method="POST" enctype="multipart/form-data">
+    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 hover:bg-gray-100">
+        
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            {#if files && files.length}
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400 font-semibold">{files[0].name}</p>
+            {:else}
+            <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">PNG, JPG</p>
+            {/if}  
+        </div>
+        <input bind:files name='image' id="dropzone-file" type="file" accept="image/png, image/jpeg" class="hidden" />
+    </label>
+    <div class="mb-3">
+        <label for='username'>Username</label>
+        <input name='username' id='username' type='text' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" />
+    </div>
+    <div class="mb-3">
+        <label for='content'>Content</label>
+        <textarea name='content' id='content' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"></textarea>
+    </div>
+    <button type='submit' class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">Share</button>
+</form>
