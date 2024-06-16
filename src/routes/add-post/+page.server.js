ba1108f0c9682ca0a736d02f99d1bd115440f667
Sarthak0205 <codeSarthak02@gmail.com>
2024-06-16 17:53:59 +0530

    Instagram_clone

---
+++ src/routes/add-post/+page.server.js
@@ -0,0 +1,20 @@
+import { redirect } from "@sveltejs/kit";
+import prisma from "$lib/prisma";
+
+export const actions = {
+    default: async({ request })=>{
+        const data = Object.fromEntries(await request.formData());
+let username = data.username;
+let caption = data.caption;
+let image = Buffer.from(await data.image.arrayBuffer()).toString('base64');
+    
+        await prisma.post.create({
+            data:{
+                username,
+                caption,
+                image
+            }
+        })
+        throw redirect(302,"/");
+    }
+}
