512c6a1cd4278b63bc59d52725058fbf87eac7d3
Alistair Dsouza <alistair2522@gmail.com>
2024-06-16 19:35:45 +0530

    minor changes

--- src/routes/add-post/+page.server.js
+++ src/routes/add-post/+page.server.js
@@ -1,21 +1,34 @@
-import prisma from "$lib/prisma";
 import { redirect } from "@sveltejs/kit";
+import prisma from "../../lib/prisma.js";
 
-export const actions ={
+export const actions = {
     default: async ({ request }) => {
         const data = Object.fromEntries(await request.formData());
-        let username = data.username;
-        let content = data.content;
-        let image = Buffer.from(await data.image.arrayBuffer()).toString('base64');
+        const username = data.username;
+        const content = data.content;
+
+        // Original image data (assuming it's uploaded normally)
+        let image = Buffer.from(await data.image.arrayBuffer()).toString(
+            "base64"
+        );
+
+        // Filtered image data from canvas
+        let filteredImageData = null;
+        if (data.filteredImageData) {
+            filteredImageData = data.filteredImageData.split(";base64,")[1];
+        }
+
+        // Determine which image data to use for upload
+        const imageDataToUpload = filteredImageData || image;
 
         await prisma.post.create({
             data: {
                 username,
                 content,
-                image
-            }
-        })
+                image: imageDataToUpload,
+            },
+        });
 
-        throw redirect(302, "/");
-    }
-}
+        return redirect(302, "/");
+    },
+};
