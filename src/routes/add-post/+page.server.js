8280a40741909e5a0660bee8a0e2094e20fb5f2b
Ramcharan <ramcharan2k4@gmail.com>
2024-06-18 17:35:00 +0530

    Done all tasks including additional tasks

---
+++ src/routes/add-post/+page.server.js
@@ -0,0 +1,22 @@
+import { redirect } from "@sveltejs/kit";
+import prisma from "$lib/prisma";
+
+export const actions = {
+  default: async ({ request }) => {
+    const data = Object.fromEntries(await request.formData());
+    console.log(data);
+    let username = data.username;
+    let content = data.content;
+    let image = Buffer.from(await data.image.arrayBuffer()).toString("base64");
+
+    await prisma.post.create({
+      data: {
+        username,
+        content, 
+        image,
+      },
+    });
+
+    throw redirect(302, "/");
+  },
+};
