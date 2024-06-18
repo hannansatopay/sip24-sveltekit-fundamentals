823e1e04fbecb32999421500d15fe30e2027d56d
Vaishnavi <vaishnavirashankar18@gmail.com>
2024-06-18 17:04:00 +0530

    Task completed successfully

---
+++ src/routes/add-post/+page.svelte
@@ -0,0 +1,81 @@
+<script>
+  let files = null;
+  $: console.log(files);
+</script>
+
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+  <div class="container mx-auto px-4 flex justify-between items-center">
+    <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">CraftLab</h1>
+    <a
+      href="/"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >Home</a
+    >
+  </div>
+</header>
+
+<form class="container mx-auto p-5" method="POST" enctype="multipart/form-data">
+  <label
+    for="dropzone"
+    class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
+  >
+    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+      {#if files && files.length}
+        <p class="text-sm text-gray-500 font-semibold">{files[0].name}</p>
+      {:else}
+        <svg
+          class="w-8 h-8 mb-4 text-gray-500"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M7 16v4h10v-4M12 12v8m0-12V4m0 0L8.5 7M12 4l3.5 3"
+          />
+        </svg>
+        <p class="text-sm text-gray-500 font-semibold">Click to upload</p>
+      {/if}
+    </div>
+    <input
+      bind:files
+      name="image"
+      id="dropzone"
+      type="file"
+      accept="image/png, image/jpeg"
+      class="hidden"
+      required
+    />
+  </label>
+
+  <div class="mb-4">
+    <label for="username" class="block text-sm font-medium text-gray-900"
+      >Username</label
+    >
+    <input
+      type="text"
+      id="username"
+      name="username"
+      class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+    />
+  </div>
+  <div class="mb-4">
+    <label for="username" class="block text-sm font-medium text-gray-900"
+      >Content</label
+    >
+    <textarea
+      id="content"
+      name="content"
+      class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+    ></textarea>
+  </div>
+  <button
+    type="submit"
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >Share</button
+  >
+</form>
