60f8016f52bc5f611605825e594b1af1e36af5c4
Ankit Sharma <123965682+Ankitsharma32@users.noreply.github.com>
2024-06-18 13:37:13 +0530

    Additional task part
    Implemented the additional task of applying filter using html5 canvas.

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,12 @@
-import prisma from '$lib/prisma';
+import prisma from "../lib/prisma";
 
-export const load = (async() => {
-    const result = await prisma.post.findMany();
-
-    return { posts: result };
-})
+export const load = async () => {
+  const result = await prisma.post.findMany({
+    orderBy:[
+      {
+        createdAt: 'desc',
+      },
+    ]
+  });
+  return { posts: result };
+};
