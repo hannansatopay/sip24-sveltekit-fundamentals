46904b72da7a54a2deb600e11f0c59d576f4efbd
MelissaSequeira <melissasequeira572@gmail.com>
2024-06-18 23:40:19 +0530

    additional tasks done by melissa

--- src/routes/add-post/+page.svelte
+++ src/routes/add-post/+page.svelte
@@ -1,34 +1,136 @@
-
 <script>
-    let files= null;
+    import { onMount } from 'svelte';
+    import { browser } from '$app/environment';
 
+    let files = null;
+    let canvas;
+    let ctx;
+    let img;
+    let originalImageData;
+
+    if (browser) {
+        img = new Image();
+    }
+
+    onMount(() => {
+        if (browser) {
+            canvas = document.querySelector('canvas');
+            ctx = canvas.getContext('2d');
+        }
+    });
+
+    function handleFileUpload(event) {
+        if (browser) {
+            files = event.target.files;
+            if (files.length > 0) {
+                const reader = new FileReader();
+                reader.onload = function (e) {
+                    img.src = e.target.result;
+                    img.onload = () => {
+                        canvas.width = img.width;
+                        canvas.height = img.height;
+                        ctx.drawImage(img, 0, 0);
+                        originalImageData = ctx.getImageData(0, 0, canvas.width, canvas.height); // Save original image data
+                    };
+                };
+                reader.readAsDataURL(files[0]);
+            }
+        }
+    }
+
+    function applyFilter(filterType) {
+        if (browser) {
+            const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+            const data = imageData.data;
+            for (let i = 0; i < data.length; i += 4) {
+                const r = data[i];
+                const g = data[i + 1];
+                const b = data[i + 2];
+                if (filterType === 'grayscale') {
+                    const avg = 0.3 * r + 0.59 * g + 0.11 * b;
+                    data[i] = data[i + 1] = data[i + 2] = avg;
+                } else if (filterType === 'sepia') {
+                    data[i] = r * 0.393 + g * 0.769 + b * 0.189;
+                    data[i + 1] = r * 0.349 + g * 0.686 + b * 0.168;
+                    data[i + 2] = r * 0.272 + g * 0.534 + b * 0.131;
+                }
+            }
+            ctx.putImageData(imageData, 0, 0);
+        }
+    }
+
+    function saveImage() {
+        if (browser) {
+            const link = document.createElement('a');
+            link.download = 'filtered-image.png';
+            link.href = canvas.toDataURL();
+            link.click();
+        }
+    }
+
+    function resetImage() {
+        if (browser) {
+            ctx.putImageData(originalImageData, 0, 0); // Restore original image data
+        }
+    }
 </script>
-<header class ="bg-white py-4 shadow-md sticky top-0 z-10">
+
+<style>
+    .bg-custom-blue {
+        background-color: #bbd4de8f;
+    }
+
+    .text-custom-white {
+        color: #FFFFFF;
+    }
+
+    .bg-custom-blue-darker {
+        background-color: #094869;
+    }
+
+    .text-custom-blue-darker {
+        text-color: #094869;
+    }
+</style>
+
+<header class="bg-opacity-75 bg-custom-white py-4 shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex justify-between items-center">
         <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
-        <a href="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Home</a>
+        <a href="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Home</a>
     </div>
 </header>
 
-<form class="container mx-auto p-5" method="POST" enctype="multipart/form-data">
-    <label for="dropzone" class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"> 
+<form class="container mx-auto p-5 bg-white rounded-lg shadow-md mt-6" method="POST" enctype="multipart/form-data">
+    <label for="dropzone" class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50">
         <div class="flex flex-col items-center justify-center pt-5 pb-6">
             {#if files && files.length}
                 <p class="text-sm text-gray-500 font-semibold">{files[0].name}</p>
             {:else}
-                <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/></svg>
+                <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                </svg>
                 <p class="text-sm text-gray-500 font-semibold">Click to upload</p>
             {/if}
         </div>
-        <input bind:files name="image" id="dropzone" type="file" accept="image/png, image/jpeg" class="hidden" required/>
+        <input bind:files name="image" id="dropzone" type="file" accept="image/png, image/jpeg" class="hidden" required on:change={handleFileUpload}/>
     </label>
     <div class="mb-3">
         <label for="username" class="block mb-2 text-sm font-medium text-gray-900">Username</label>
-        <input name="username" id="username" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"/>
+        <input name="username" id="username" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5" />
     </div>
     <div class="mb-3">
         <label for="content" class="block mb-2 text-sm font-medium text-gray-900">Content</label>
-        <textarea name="content" id="content" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"></textarea>
+        <textarea name="content" id="content" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"></textarea>
     </div>
-    <button type="submit" class="bg-blue-800 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Share</button>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">Share</button>
 </form>
+
+<div class="container mx-auto p-5 bg-white rounded-lg shadow-md mt-6">
+    <canvas class="border w-full"></canvas>
+    <div class="flex justify-center mt-3">
+        <button on:click={() => applyFilter('grayscale')} class="bg-gray-500 hover:bg-gray-700 text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2">Grayscale</button>
+        <button on:click={() => applyFilter('sepia')} class="bg-yellow-500 hover:bg-yellow-700 text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2">Sepia</button>
+        <button on:click={resetImage} class="bg-blue-500 hover:bg-blue-700 text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2">Original</button>
+        <button on:click={saveImage} class="bg-green-500 hover:bg-green-700 text-white font-medium rounded-lg text-sm px-5 py-2.5">Save on your device</button>
+    </div>
+</div>
