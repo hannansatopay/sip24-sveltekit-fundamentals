d013017bd004e7572f53d57031318cf31b49850d
Muhaimin Nafim <92179931+Nafim16@users.noreply.github.com>
2024-06-19 16:20:52 +0600

    CR to prisma

--- src/routes/add-post/+page.server.js
+++ src/routes/add-post/+page.server.js
@@ -1,7 +1,26 @@
+import prisma from '$lib/prisma';
+import { redirect } from '@sveltejs/kit';
+
+
 export const actions = {
     default: async ({ request }) => {
-        const data = await Object.fromEntries(request.formData());
 
-        console.log(data);
+        const formData = await request.formData();
+        const data = Object.fromEntries(formData.entries());
+        // const data = await Object.fromEntries(request.formData.entries())
+
+        let username = data.username;
+        let content = data.content;
+        let image = Buffer.from(await data.image.arrayBuffer()).toString('base64');
+
+        await prisma.post.create({
+            data: {
+                username,
+                content,
+                image
+            }
+        });
+
+        throw redirect(302, "/");
     }
 }
