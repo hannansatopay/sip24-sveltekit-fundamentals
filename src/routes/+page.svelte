d96d79c7b7d5281651c06ced1379691fad45166f
US30 <utkarsh.sinha@somaiya.edu>
2024-06-19 13:09:59 +0530

    ui changes
    made ui changes and added more test pics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 
 <script>
-    export let data;
+  export let data;
 </script>
 <header class="bg-white py-4 shadow-md sticky top-0 z-10">
 <div class="container mx-auto px-4 flex justify-between items-center">
@@ -10,15 +10,15 @@
 </header>
 
 <div class="container mx-auto md:my-5">
-  <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-    {#each data.posts as post}
-    <div>
-        <img class="w-full" src="data:image;base64,{post.image}" alt="Post">
-        <div class="text-sm py-2">
-            <span class="font-bold">{post.username}</span>
-            <span class="text-gray-700">{post.content}</span>
-        </div>
-    </div>
-    {/each}
+<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+  {#each data.posts.slice().reverse() as post}
+  <div>
+      <img class="w-full" src="data:image;base64,{post.image}" alt="Post">
+      <div class="text-sm py-2">
+          <span class="font-bold">{post.username}</span>
+          <span class="text-gray-700">{post.content}</span>
+      </div>
   </div>
+  {/each}
+</div>
 </div>
