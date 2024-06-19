03568c501be66cf0c0a96a5781e1c66aad336129
Swastika Soni <140913627+SwastikaSoni@users.noreply.github.com>
2024-06-19 23:56:17 +0530

    Swastika Soni

---
+++ src/routes/add-post/+page.svelte
@@ -0,0 +1,132 @@
+<script>
+  let files = null;
+  let selectedFilter = "original";
+  function handleFileChange(event) {
+    const fileList = event.target.files;
+    if (fileList.length > 0) {
+      files = fileList;
+    } else {
+      files = null;
+    }
+  }
+  function applyFilter(event) {
+    selectedFilter = event.target.value;
+    const hiddenInput = document.getElementById("filterInput");
+    hiddenInput.value = selectedFilter;
+  }
+</script>
+
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+  <div class="container mx-auto px-4 flex justify-between items-center">
+    <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+    <a
+      href="/"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >Home</a
+    >
+  </div>
+</header>
+
+<form class="container mx-auto p-5" method="POST" enctype="multipart/form-data">
+  <label
+    for="dropzone"
+    class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50"
+  >
+    <div class="upload-container">
+      {#if files && files.length > 0}
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img
+          src={URL.createObjectURL(files[0])}
+          alt="Uploaded Image"
+          class="uploaded-image mb-4"
+          style={selectedFilter === "grayscale"
+            ? "filter: grayscale(100%);"
+            : selectedFilter === "sepia"
+              ? "filter: sepia(100%);"
+              : ""}
+        />
+        <p class="text-sm text-gray-500 font-semibold">{files[0].name}</p>
+      {:else}
+        <svg
+          class="w-8 h-8 mb-4 text-gray-500"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 16"
+          ><path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+          /></svg
+        >
+        <p class="text-sm text-gray-500 font-semibold">Click to upload</p>
+      {/if}
+    </div>
+    <input
+      bind:files
+      name="image"
+      id="dropzone"
+      type="file"
+      accept="image/png, image/jpeg"
+      class="hidden"
+      required
+      on:change={handleFileChange}
+    />
+  </label>
+  <div class="filter-dropdown">
+    <select class="text-sm" bind:value={selectedFilter} on:change={applyFilter}>
+      <option value="original">Original</option>
+      <option value="grayscale">Grayscale</option>
+      <option value="sepia">Sepia</option>
+    </select>
+  </div>
+  <div class="mb-3">
+    <label for="username" class="block mb-2 text-sm font-medium text-gray-900"
+      >Username</label
+    >
+    <input
+      name="username"
+      id="username"
+      type="text"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+    />
+  </div>
+  <div class="mb-3">
+    <label for="content" class="block mb-2 text-sm font-medium text-gray-900"
+      >Content</label
+    >
+    <textarea
+      name="content"
+      id="content"
+      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+    ></textarea>
+  </div>
+  <button
+    type="submit"
+    class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"
+    >Share</button
+  >
+</form>
+
+<style>
+  .upload-container {
+    max-width: 100%;
+    height: auto;
+    text-align: center;
+  }
+
+  .uploaded-image {
+    max-width: 100%;
+    max-height: 250px;
+    border-radius: 8px;
+    object-fit: cover;
+  }
+  .filter-dropdown {
+    position: relative;
+    background-color: white;
+    padding: 10px;
+    z-index: 100;
+  }
+</style>
