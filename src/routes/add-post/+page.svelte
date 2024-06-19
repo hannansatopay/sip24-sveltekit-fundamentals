aadf3fae733b7b766d6945544eb3c01b79a434e5
tanuja1922 <139243492+tanuja1922@users.noreply.github.com>
2024-06-19 18:36:27 +0530

    3rd task done

---
+++ src/routes/add-post/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    let files = null;
+    $: console.log(files);
+</script>
+
+<header class="bg-white py-5 shadow-md">
+    <div class="container mx-auto px-5 flex justify-between items-center">
+        <h1 class="text-2xl font-bold font=['Conic_Sans_MS']">CraftLab</h1>
+        <a href="/" class="bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-2 ">Home</a>
+    </div>
+</header>
+
+<form class="container mx-auto p-5" method="POST" enctype="multipart/form-data">
+    <label for="dropzone" class="rb-3 flex flex-col items-center justify-center w-full h-68 border-2 border-gray-300">
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            {#if files && files.length}
+            <p class="text-sm text-gray-500 font-semibold">{files[0].name}</p>
+            {:else}
+            <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16"></svg>
+            <p class="text-sm text-gray-500 font-semibold"> Click to upload</p>
+            {/if}
+        </div>
+        <input bind:files name="image" id="dropzone" type="file" accept="image/png, image/jpeg" class="hidden" required>
+    </label>
+    <div class="mb-3">
+        <label for="username" class="block mb-2 text-sm font-redius text-gray-900">Username</label>
+        <input name="username" type="text" id="username" class="bg-gray-50 border borfer-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5">
+    </div>
+    <div class="mb-3">
+        <label for="content" class="block mb-2 text-sm font-redius text-gray-900">Content</label>
+        <textarea name="content" id="content" class="bg-gray-50 border borfer-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"></textarea>
+    </div>
+    <button type="submit" class="text-white bg-blue-500 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">Post</button>
+    
+</form>
