762c94a3bb697f710bdfceb6adca11c575c4728f
Mishra Sumit Pramod <154874510+SumitPramodMishra@users.noreply.github.com>
2024-06-20 21:43:51 +0530

    sumitsolution

---
+++ src/routes/add-post/+page.server.js
@@ -0,0 +1,18 @@
+ import { redirect, Redirect } from "@sveltejs/kit";
+export const actions ={
+    default: async ({request}) => {
+        const data = await request.formData();
+        let username = data.username;
+        let content = data.content;
+        let image = Buffer.from(await data.image,arrayBuffer()).toString('base64');
+        
+       await prisma.post.create({
+        data:{
+           username,
+           content,
+           image 
+        }
+       });
+       throw redirect(302,"/");
+    }
+}
---
+++ src/routes/add-post/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    let files =null;
+    
+
+</script>
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+        <h1 class="text-2xl font-bond font-['comic_sans_MS']">Craftlab</h1>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-blod py-2 px-4 rounded">Home</button>
+    </div>
+</header>
+<form class="container mx-auto p-5" method="post" enctype="multipart/form-data">
+    <label for="dropzone" class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300">
+    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+        {#if files && files.length}
+        <p class="text-sm text-gray-500 font-semibold">{files[0].name}</p>
+        {:else}
+        <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewbox="0 0 20 16"></svg>
+        <p class="text-sm text-gray-500 font-semibold">Click to upload</p>
+        {/if}
+    </div>
+    <input name="image" id="dropzone" type="file" accept="image/png, image/jpeg" class="hidden" required>
+    </label>
+    <div class="mb-3">
+      <label for="username" class="block mb-2 text-5m font-medium text-gray-900">Username</label>
+      <input name="username" id="username" type="text" class ="bg-gray-50 border border-gray-300b text-gray-900 text-5m rounded-lg block w-full p-2.5"/>
+      </div>
+      <div class="mb-3">
+        <label for="content" class="block mb-2 text-5m font-medium text-gray-900">content</label>
+        <textarea name="content" id="content" type="text" class ="bg-gray-50 border border-gray-300b text-gray-900 text-5m rounded-lg block w-full p-2.5"></textarea>
+        </div>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">share</button>
+</form>
