9d1b6c5868ed5d1d1f6c07a5f3eba0acc3d09475
Umed24 <109196694+Umed24@users.noreply.github.com>
2024-06-20 15:23:18 +0530

    Added Post route

--- src/routes/add-post/+page.svelte
+++ src/routes/add-post/+page.svelte
@@ -1,6 +1,6 @@
 <header class="bg-white py-4 shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex justify-between items-center">
         <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
-        <a href="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Home</a>
+        <a href="/add-post" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add post</a>
     </div>
 </header>
