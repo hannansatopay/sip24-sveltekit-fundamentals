928f7f44317d8e8a13088a4ec0b741c984f18664
Umed24 <109196694+Umed24@users.noreply.github.com>
2024-06-20 15:44:26 +0530

    created and updated post route and basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
 <header class="bg-white py-4 shadow-md sticky top-0 z-10">
     <div class="container mx-auto px-4 flex justify-between items-center">
         <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
-        <a href="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Home</a>
+        <a href="/add-post" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add post</a>
     </div>
 </header>
+
+<div class="container mx-auto md:my-5">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        
+            <div>
+                <img class="w-full" src="image" alt="Post">
+                <div class="text-sm py-2">
+                    <span class="font-bold">username</span>
+                    <span class="text-gray-700">content</span>
+                </div>
+            </div>
+    </div>
+</div>
