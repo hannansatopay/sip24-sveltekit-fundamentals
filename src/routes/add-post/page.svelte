b11c73e026462f964c52facbe3eb830e1b15c8d4
Sambhav <sambhavbhansali10@gmail.com>
2024-06-21 21:02:44 +0530

    done

---
+++ src/routes/add-post/page.svelte
@@ -0,0 +1,14 @@
+<header class="bg-white py-4 shadow-md sticky top-0 z-10">
+    <div class="container mx-auto px-4 flexjustify-between items-center">
+     <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Sambhav's Gram</h1>
+     <a href="/" classs="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Home</a>
+    </div>    
+ </header>
+
+ <form class="container mx-auto p-5">
+    <input type='file'/>
+    <div>
+        <label>Username</label>
+        <input type="text" class="bg -gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"/>
+    </div>
+ </form>
