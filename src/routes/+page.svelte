d04376505e504d627cf2d16051701115a9aa8598
Sejal Somani <130598961+sejalsomani0504@users.noreply.github.com>
2024-06-21 14:11:03 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="bg py-4 shadow-md ">
+    <div class="container mx-auto px-4">
+
+
+    <h1 class="text-2xl font-">Craftlab</h1>
+    </div>
+
+</header>
