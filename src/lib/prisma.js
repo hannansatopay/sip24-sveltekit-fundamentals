ba6247ce26d7d00efa143219d5ebf39ec87c803e
LAB 206 <lab206@SAKEC.LOCAL>
2024-06-22 15:36:44 +0530

    Instagram Clone Done with Prisma and HTML canvas

---
+++ src/lib/prisma.js
@@ -0,0 +1,7 @@
+// import { prismaClient } from '@prisma/client'
+// const prisma = new prismaClient();
+// export default prisma;
+
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+export default prisma
