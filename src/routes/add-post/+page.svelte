cdcfc744b6e39b87e6431f50b13e6facbedd8c18
KapuluruBhuvaneswariVspdbct <kapulurubhuvaneeswarivspdbct@gmail.com>
2024-06-23 14:05:13 +0530

    changes

---
+++ src/routes/add-post/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    let files=null;
+</script>
+<header class="bg-white py-4 shadow-md">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+        <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">BLab</h1>
+        <a href="/" class="bg-blue-500  hover:bg-blue-700  text-white rounded  font-bold py-2">HOME</a>
+    </div>
+</header>
+<form class="container mx-auto p-5" method="POST" enctype="multipart/form-data">
+
+<label for="dropzone" class="mb-3 flex flex-col items-center justify-center w-full h-64 border-2 border-gray-3">
+    <div class="flex flex-col items-center justify center pt-5 pb-6">
+       {#if files && files.length}
+       <p class="text-sm text-gray-500 font-semibold">{files[0].name}</p>
+       {:else}
+        <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="https://www.w2.org/2000/svg" fill="none" viewBox="0 0 20 16">⬆️</svg>
+        <p class="text-sm text-gray-500 font-semibold">Click to upload</p>
+    {/if}
+    </div>
+    <input bind:files name="image" id="dropzone" type="file" accept="image/png ,image/jpg" class="hidden" required/>
+</label>
+<div class="mb-3">
+<label for="username" class="block mb-2 text-sm font-medium text-gray-900">USERNAME</label>
+<input name="username"id="username" type="text"  class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"/>
+</div>
+<div class="mb-3">
+    <label for="content" class="block mb-2 text-sm font-medium text-gray-900">CONTENT</label>
+    <textarea name="content"id="content" type="text"  class="bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"></textarea>
+    </div>
+<button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5">Post</button>
+</form>
