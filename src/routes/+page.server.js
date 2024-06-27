04d48a2a2708f5c0e5c93e04ccc795e710073ad0
Angela Mugwe <asonimugwe@gmail.com>
2024-06-27 19:06:01 +0300

    solution

--- src/routes/+page.server.js
+++
@@ -1,7 +0,0 @@
-import prisma from '$lib/prisma';
-
-export const load = (async() => {
-    const result = await prisma.post.findMany();
-
-    return { posts: result };
-})
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,35 @@
-<script>
-    export let data;
-</script>
-
 <header class="bg-white py-4 shadow-md sticky top-0 z-10">
-    <div class="container mx-auto px-4 flex justify-between items-center">
-        <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
-        <a href="/add-post" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add post</a>
+    <div class="container mx-auto px-4 flex justify-betweeen items-center">
+         <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+         <a href="/add-post" class="mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add post</a>
     </div>
 </header>
 
 <div class="container mx-auto md:my-5">
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        {#each data.posts as post}
-            <div>
-                <img class="w-full" src="data:image;base64,{post.image}" alt="Post">
-                <div class="text-sm py-2">
-                    <span class="font-bold">{post.username}</span>
-                    <span class="text-gray-700">{post.content}</span>
-                </div>
-            </div>
-        {/each}
-    </div>
+<!-- Post Template-->
+       <div>
+        <img class="w-full" src="https://picsum.photos/600" alt="Post">
+         <div class="text-sm py-2">
+       <span class="font-bold">Username</span> 
+       <span class="text-gray-700">Lorem Ipsumer Doler Ismet Amet</span> 
+         </div>
+       </div>
+
+<div>
+    <img class="w-full" src="https://picsum.photos/600" alt="Post">
+     <div class="text-sm py-2">
+   <span class="font-bold">Username</span> 
+   <span class="text-gray-700">Lorem Ipsumer Doler Ismet Amet</span> 
+     </div>
+   </div>
+
+   <div>
+    <img class="w-full" src="https://picsum.photos/600" alt="Post">
+     <div class="text-sm py-2">
+   <span class="font-bold">Username</span> 
+   <span class="text-gray-700">Lorem Ipsumer Doler Ismet Amet</span> 
+     </div>
+   </div>
+</div>
 </div>
