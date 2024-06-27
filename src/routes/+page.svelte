27d4683e04daed204108e0845b8a339a3b1a75ff
sakshi <55422286+sakshibarbade@users.noreply.github.com>
2024-06-27 17:56:49 +0530

    sakshi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,33 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="bg-white py-4 shadows-md sticky top-0 z-10">
+    <div class="container mx-auto px-4 flex justify-between items-center">
+        <h1 class="text-2xt font-bold font-['Comic_Sans_MS']">Craftlab</h1>
+        <a href="/add-post" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Post</a>
+    </div>
+</header>
+
+<div class="container mx-auto md:my-5">
+<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+    <!-- post templet -->
+<div>
+<img class="w-full" src="https://picsum.photos/600" alt="Post">
+<div class="text-sm py-2">
+<span class="font-bold">Username</span>
+<span class="text-grey-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. </span>
+</div>
+</div>
+<div>
+    <img class="w-full" src="https://picsum.photos/600" alt="Post">
+    <div class="text-sm py-2">
+    <span class="font-bold">Username</span>
+    <span class="text-grey-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. </span>
+    </div>
+    </div>
+    <div>
+        <img class="w-full" src="https://picsum.photos/600" alt="Post">
+        <div class="text-sm py-2">
+        <span class="font-bold">Username</span>
+        <span class="text-grey-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. </span>
+        </div>
+        </div>
+</div>
+</div>
