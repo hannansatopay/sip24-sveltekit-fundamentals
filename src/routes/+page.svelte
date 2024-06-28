c138ef3f6867adea514e226d7e727e5a23dda3d7
navneet <navneetsharma2302@gmail.com>
2024-06-28 12:11:42 +0530

    upp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <header class="bg-white py-4 shadow-md sticky top-0 z-10">
   <div class="container mx-auto px-4 flex justify-between items-center">
-    <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">CraftLab</h1>
+    <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">Apna PostHub</h1>
     <a
       href="/add-post"
       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
