5adaac4f5216ef105d78519c9f0bb4a267cd74d1
Nikita Kumari <130145525+nikita7526@users.noreply.github.com>
2024-06-28 13:09:45 +0530

    Instagram_Clone using Sveltekit

---
+++ prisma/migrations/20240628070035_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "image" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
