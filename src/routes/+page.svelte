d21df8cb5c48677b4f6f56c8a4a8a7b09935d321
Soumya Ranjan Tripathy <sonutripathy36@gmail.com>
2024-06-30 14:54:46 +0530

    Soumya
    Sonu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,38 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="bg-white py-4 shadow-md stricky top-0 z-10">
+    <div class =" container mx-auto px-4 flex justify-between items-center">
+
+    </div>
+    <h1 class="text-2xl font-bold font-['Comic_Sans_MS]">
+        Craftlab
+    </h1>
+    <a href="/add-post" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add post</a>
+</header>
+
+<div class="container mx-auto md:my-5">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div>
+            <img class="w-full" src="https://th.bing.com/th/id/R.9bcf216ba66d755d01307dd6e62e58b4?rik=Jrd94WgAuN8Ohw&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fc%2f7%2f1%2f460702.jpg&ehk=Nv7Yrbgxp8%2feoSXfYXHknPBkG%2bGt7Ytdwg%2bl0nYhpi0%3d&risl=&pid=ImgRaw&r=0" alt="Post">
+            <div class="text-sm py-2">
+                <span class="font-bold"> Username </span>
+                <span class="text-gray-700"> Lorem Ipsumer Doler Ismet Amet </span>
+            </div>
+        </div>
+
+        <div>
+            <img class="w-full" src="https://th.bing.com/th/id/R.9bcf216ba66d755d01307dd6e62e58b4?rik=Jrd94WgAuN8Ohw&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fc%2f7%2f1%2f460702.jpg&ehk=Nv7Yrbgxp8%2feoSXfYXHknPBkG%2bGt7Ytdwg%2bl0nYhpi0%3d&risl=&pid=ImgRaw&r=0" alt="Post">
+            <div class="text-sm py-2">
+                <span class="font-bold"> Username </span>
+                <span class="text-gray-700"> Lorem Ipsumer Doler Ismet Amet </span>
+            </div>
+        </div>
+
+        <div>
+            <img class="w-full" src="https://th.bing.com/th/id/R.9bcf216ba66d755d01307dd6e62e58b4?rik=Jrd94WgAuN8Ohw&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fc%2f7%2f1%2f460702.jpg&ehk=Nv7Yrbgxp8%2feoSXfYXHknPBkG%2bGt7Ytdwg%2bl0nYhpi0%3d&risl=&pid=ImgRaw&r=0" alt="Post">
+            <div class="text-sm py-2">
+                <span class="font-bold"> Username </span>
+                <span class="text-gray-700"> Lorem Ipsumer Doler Ismet Amet </span>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ src/routes/Add-post/+page.svelte
@@ -0,0 +1,19 @@
+<header class="bg-white py-4 shadow-md stricky top-0 z-10">
+    <div class =" container mx-auto px-4 flex justify-between items-center">
+        <h1 class="text-2xl font-bold font-['Comic_Sans_MS']">
+            Craftlab
+        </h1>
+        <a href="/" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Home</a>
+
+    </div>
+
+</header>
+
+<form class="container mx-ato p-5">
+    <input type="file" />
+    <div>
+        <label>Username</label>
+        <input type="text" class="bg-gray-50 border boreder-gray-900 text-sm rounded-lg block w-full p-2.5 "/>
+    </div>
+    
+</form>
