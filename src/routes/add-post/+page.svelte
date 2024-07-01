fe86ed88287a06d2efb5a730c4e2be905e4b8f21
Kishankumar57 <145453142+Kishankumar57@users.noreply.github.com>
2024-07-01 10:20:36 +0530

    Kishan Kumar Behera
    Completed

--- src/routes/add-post/+page.svelte
+++ src/routes/add-post/+page.svelte
@@ -34,7 +34,7 @@
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
             </svg>
             <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-            <p class="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p> -->
+            <p class="text-xs text-gray-500 dark:texgray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p> -->
         </div>
         <input bind:files id="dropzone" type="file" name="image" class="hidden" accept="png/jpg" />
     </label>
